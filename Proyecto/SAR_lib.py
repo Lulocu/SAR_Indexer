2afe7f450a2e084cc95a4725b3bd56d34d71cae9
UzuRodri95 <63551890+UzuRodri95@users.noreply.github.com>
2020-05-21 18:40:52 +0200

    Add files via upload

--- Proyecto/SAR_lib.py
+++ Proyecto/SAR_lib.py
@@ -521,8 +521,8 @@ class SAR_Project:
             return []
 
         consultaPartes = query.split()
-        #print(consultaPartes)
-        
+        print("Consulta partes: ",consultaPartes)
+
         j = 0
 
         while j <len(consultaPartes):
@@ -580,12 +580,11 @@ class SAR_Project:
                     i = i + 1
                     res = self.or_posting(res,self.reverse_posting(listaPosting[i]))
                 else:
-                    
+
                     res = self.or_posting(res,listaPosting[i])
-          
+
             i = i + 1
-        
-        print(res)
+
         return res
 
 
@@ -815,7 +814,7 @@ class SAR_Project:
         result = self.solve_query(query)
 
         if self.use_ranking:
-            result = self.rank_result(result, query)
+            ranking = self.rank_result(result, query)
 
         print("=" * 20)
         print("Query: '" + query + "'")
@@ -860,6 +859,114 @@ class SAR_Project:
                     target = noticia
                     break
 
+            #for termino
+    def parentesis(self,consultaPartes):
+        #Inicialización variables
+        encontrado = True
+        Boperador = False
+        Boperador2 = False
+        NOToperador = False
+        #print('-' * 50)
+        #print(consultaPartes)
+
+        #encontrado: para cuando haya más de un paréntesis
+        while encontrado:
+            encontrado = False
+            seguir = True
+            #print(consultaPartes)
+            #recorremos todas las palabras mirando si empiezan por paréntesis
+            for j in range(len(consultaPartes)):
+                palabra = consultaPartes[j]
+                if seguir and palabra[0] == '(':
+                    encontrado = True
+                    seguir = False
+                    #Empieza por '(' y antes tiene un operador booleano != 'NOT
+                    #Guardamos el operador
+                    if j -1 >= 0 and consultaPartes[j-1] in ['AND','OR']:
+
+                        operador = consultaPartes[j-1]
+                        Boperador = True
+                    #Empieza por '(' y antes tiene un NOT, hay que negar
+                    #todo lo de dentro y aplicar morgan
+                    elif j-1 >= 0 and consultaPartes[j-1] == 'NOT':
+                        #operador = consultaPartes[j-1]
+                        NOToperador = True
+
+                        #Guardar el operador anterior al NOT
+                        if j -2 >= 0 and consultaPartes[j-2] in ['AND','OR','NOT']:
+                            operador2 = consultaPartes[j-2]
+                            Boperador2 = True
+
+                    #Guardamo indice de donde empiza el paréntesis y quitamos el paréntesis
+                    inicio = consultaPartes.index(palabra)
+                    letra = palabra.replace('(','')
+
+                    #buscamos el último cierre de paréntesis
+                    for fin in reversed(consultaPartes):
+                         if fin[-1] == ')':
+                            final = consultaPartes.index(fin)
+                            letraF = fin.replace(')','')
+
+                    #Ailamos lo que estaba entre paréntesis y le quitamos los paréntesis
+                    medio = [letra] + consultaPartes[inicio + 1:final] + [letraF]
+                    if Boperador:
+                        medio = medio + [operador]
+                    #Hay un NOT antes del paréntesis, negamos lo de dentro
+                    if NOToperador:
+
+                        #Negamos todo lo que haya dentro del paréntesis y lo reconvertimos en una lista
+                        medio = '.NOT.'.join(medio)
+                        medio = medio.split('.')
+                        medio = ['NOT'] + medio
+
+                        #Aplicamos la ley de morgan sobre los OR
+                        #Buscamos un OR
+                        aplMorgan = True
+                        while aplMorgan:
+                            try:
+                                pos = medio.index('OR')
+                                if medio[pos-1] == 'NOT':
+                                    #Se añade con . para evitar bucles infinitos en la siguiente comprobacion
+                                    medio = medio[0:pos -1] + [".AND"] + medio[pos+1:]
+                            except:
+                               aplMorgan = False
+
+                        aplMorgan = True
+                        while aplMorgan:
+                            try:
+                                pos = medio.index('AND')
+                                if medio[pos-1] == 'NOT':
+                                    medio = medio[0:pos -1] + ['OR'] + medio[pos+1:]
+                            except:
+                                aplMorgan = False
+
+                        #Quita el . del AND
+                        for idx, item in enumerate(medio):
+                            if item == '.AND':
+                                medio[idx] = 'AND'
+                        break
+
+            if(encontrado):
+                if Boperador2:
+                    #print(1)
+                    #ponemos lo que estaba entre paréntesis a la derecha, añadimos el operador anterior al paréntesis
+                    consultaPartes = medio + [operador2] + consultaPartes[0:inicio - 2] + consultaPartes[final+1:]
+                elif NOToperador:
+                    #print("2")
+                    consultaPartes = medio  + consultaPartes[0:inicio-1] +consultaPartes[final+1:] #
+                else:
+                    #print("3")
+
+                    consultaPartes = medio + consultaPartes[0:inicio] + consultaPartes[final+1:]
+            Boperador = False
+            Boperador2 = False
+            NOToperador = False
+        #print(consultaPartes)
+        return consultaPartes
+
+        #fin tratamiento paréntesis
+
+
     def rank_result(self, result, query):
         """
         NECESARIO PARA LA AMPLIACION DE RANKING
@@ -869,8 +976,4 @@ class SAR_Project:
         return: la lista de resultados ordenada
         """
 
-        return 0
-
-        ###################################################
-        ## COMPLETAR PARA FUNCIONALIDAD EXTRA DE RANKING ##
-        ###################################################
+        return []
